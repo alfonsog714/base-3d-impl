0487474b1114507086ce3ec77322247bb000607f
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2025-03-20 11:02:26 -0700

    use SDL's true delay function

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_timer.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -60,9 +61,15 @@ void process_input(void)
 
 void update(void)
 {
-	while (!SDL_TICKS_PASSED(SDL_GetTicks(),
-				 previous_frame_time + FRAME_TARGET_TIME)) {
-		;
+	// while (!SDL_TICKS_PASSED(SDL_GetTicks(),
+	// 			 previous_frame_time + FRAME_TARGET_TIME)) {
+	// 	;
+	// }
+
+	int time_to_wait =
+	    FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
+	if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
+		SDL_Delay(time_to_wait);
 	}
 
 	previous_frame_time = SDL_GetTicks();
