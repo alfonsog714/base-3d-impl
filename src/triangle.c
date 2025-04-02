736a9a7f26163925553cd147697669f81d93446c
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2025-04-02 09:15:45 -0700

    avoid division by zero in drawing triangles

--- src/triangle.c
+++ src/triangle.c
@@ -80,10 +80,17 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
 		int_swap(&x0, &x1);
 	}
 
-	// calculate mid point of triangle
-	int m_y = y1;
-	int m_x = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+	if (y1 == y2) {
+		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+	} else if (y0 == y1) {
+		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+	} else {
+		// calculate mid point of triangle
+		int m_y = y1;
+		int m_x =
+		    ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
 
-	fill_flat_bottom_triangle(x0, y0, x1, y1, m_x, m_y, color);
-	fill_flat_top_triangle(x1, y1, m_x, m_y, x2, y2, color);
+		fill_flat_bottom_triangle(x0, y0, x1, y1, m_x, m_y, color);
+		fill_flat_top_triangle(x1, y1, m_x, m_y, x2, y2, color);
+	}
 }
